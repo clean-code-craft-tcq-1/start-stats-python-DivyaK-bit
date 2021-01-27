69a097a36178b0a3ae076d1927e1e364a9b5112d
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-01-27 09:30:11 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -9,11 +9,14 @@ class StatsTest(unittest.TestCase):
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
-  def test_avg_is_nan_for_empty_input(self):
+ def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
     # Use nan and isnan in https://docs.python.org/3/library/math.html
 
   def test_raise_alerts_when_max_above_threshold(self):
@@ -24,6 +27,25 @@ class StatsTest(unittest.TestCase):
     statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
     self.assertTrue(emailAlert.emailSent)
     self.assertTrue(ledAlert.ledGlows)
+    
+class EmailAlert:
+  emailSent = False
 
+    
+class LEDAlert:
+  ledGlows = False
+    
+
+class StatsAlerter(EmailAlert,LEDAlert):
+  maxThreshold = None
+  def __init__(self,maxThreshold, p_desti):
+    self.maxThreshold = maxThreshold
+    self.p_desti = p_desti
+    
+  def checkAndAlert(self, p_desti):
+    if max(p_desti) > self.maxThreshold:
+      self.p_desti[0].emailSent=True
+      self.p_desti[1].ledGlows=True
+ 
 if __name__ == "__main__":
   unittest.main()
